44038891462e7c6257c5dc37d1b64f46a9def41e
Jon Grythe Stødle <jonstodle@outlook.com>
2019-08-28 23:40:18 +0200

    Simplify javascript and make page show without javascript enabled

--- public/js/index.js
+++ public/js/index.js
@@ -1,135 +1,23 @@
 (function () {
-        const talkListElement = document.getElementById('talk-list');
-        let talks = [
-            {
-                "name": "Intro to Docker",
-                "date": "August 2018",
-                "resources": [
-                    {
-                        "title": "Code",
-                        "link": "https://github.com/jonstodle/WebDevDocker"
-                    }
-                ],
-                "expanded": false
-            },
-            {
-                "name": "RxJS",
-                "date": "May 2017",
-                "resources": [
-                    {
-                        "title": "Slides",
-                        "link": "realm/index.html"
-                    },
-                ],
-                "expanded": false
-            },
-            {
-                "name": "Realm",
-                "date": "March 2017",
-                "resources": [
-                    {
-                        "title": "Slides",
-                        "link": "https://slides.com/jonstodle/realm-eng#/"
-                    },
-                    {
-                        "title": "Code",
-                        "link": "https://github.com/jonstodle/RealmTodo"
-                    },
-                ],
-                "expanded": false
-            },
-        ];
+    const talkHeaders = document.getElementsByClassName('talk-details-header');
+    const talkDetailContainers = document.getElementsByClassName('talk-details-detail-container');
+    const talkDetailHeaderExpandIndicators = document.getElementsByClassName('talk-details-header-expand-indicator');
+    const talkDetailContainerStatuses = [];
 
-        function createElement(name, attributes, children, handlers) {
-            const element = document.createElement(name);
-
-            if (!!attributes) {
-                for (const key of Object.keys(attributes)) {
-                    element.setAttribute(key, attributes[key]);
-                }
-            }
-
-            if (!!children) {
-                for (const child of children) {
-                    if (typeof child === 'string') {
-                        element.appendChild(document.createTextNode(child))
-                    } else {
-                        element.appendChild(child);
-                    }
-                }
-            }
-
-            if (!!handlers) {
-                for (const eventName of Object.keys(handlers)) {
-                    element.addEventListener(eventName, handlers[eventName]);
-                }
-            }
-
-            return element;
+    function update() {
+        for (let i = 0; i < talkDetailContainers.length; i++) {
+            const expand = !!talkDetailContainerStatuses[i];
+            talkDetailHeaderExpandIndicators[i].textContent = expand ? '▲' : '▼';
+            talkDetailContainers[i].style = expand ? "display: flex" : "display: none";
         }
-
-        function createImageDetailsItems(talkList) {
-            let items = [];
-
-            for (const talk of talkList) {
-                const talkDetails = [
-                    createElement(
-                        'div',
-                        {"class": "talk-details-header"},
-                        [
-                            createElement('h2', {"class": "talk-details-header-title"}, [talk.name]),
-                            createElement('span', {"class": "talk-details-header-date"}, [talk.date]),
-                            createElement('span', null, [talk.expanded ? '\u25B2' : '\u25BC'])
-                        ],
-                        {
-                            "click": function () {
-                                talk.expanded = !talk.expanded;
-                                render();
-                            }
-                        }
-                    )
-                ];
-
-                if (talk.expanded && !!talk.resources) {
-                    let details = [];
-
-                    for (const resource of talk.resources) {
-                        details.push(createElement(
-                            'a',
-                            {
-                                "class": "talk-details-link",
-                                "href": resource.link
-                            },
-                            [resource.title]
-                        ));
-                    }
-
-                    talkDetails.push(createElement(
-                        'div',
-                        {"class": "talk-details-detail-container"},
-                        details
-                    ));
-                }
-
-                items.push(createElement(
-                    'section',
-                    {"class": "talk-details-container"},
-                    talkDetails
-                ));
-            }
-
-            return items;
-        }
-
-        function render() {
-            talkListElement.innerHTML = "";
-
-            for (const item of createImageDetailsItems(talks)) {
-                talkListElement.appendChild(item);
-            }
-        }
-
-        render();
     }
 
-)();
+    for (let i = 0; i < talkHeaders.length; i++) {
+        talkHeaders[i].addEventListener('click', function () {
+            talkDetailContainerStatuses[i] = !talkDetailContainerStatuses[i];
+            update();
+        });
+    }
+
+    update();
+})();

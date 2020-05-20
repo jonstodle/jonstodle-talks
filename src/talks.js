e2299187e28f08e0094b7fd96b4dbd8b890e9679
Jon Grythe Stødle <jonstodle@outlook.com>
2020-05-20 14:41:03 +0200

    Add talk
    Adds "Getting Started with Svelte - Cybernetically Enhanced Web Apps"

--- src/talks.js
+++ src/talks.js
@@ -1,5 +1,19 @@
 const talks = [
     {
+        title: 'Getting Started with Svelte - Cybernetically Enhanced Web Apps',
+        date: 'May 2020',
+        links: [
+            {
+                title: 'Slides',
+                link: 'https://drive.google.com/file/d/15L1vvQFSvl1Zhmr4SLI_kWmJTe0Jh3Wa/view?usp=sharing'
+            },
+            {
+                title: 'Video',
+                link: 'https://youtu.be/eK27uMco3UI'
+            },
+        ]
+    },
+    {
         title: 'Take off with Flutter',
         date: 'November 2018',
         links: [

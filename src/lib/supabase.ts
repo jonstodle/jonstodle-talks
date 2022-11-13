7f7047c40827d2552e72b21683269779955b5e16
Jon Grythe Stødle <jon@stodle.net>
2022-11-13 01:15:49 +0100

    Remove old project

--- src/lib/supabase.ts
+++
@@ -1,3 +0,0 @@
-export const apiUrl = 'https://vxnnwdqdzmlfeihwhhnk.supabase.co';
-export const apiKey =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzODUzNzUyMywiZXhwIjoxOTU0MTEzNTIzfQ.epyCVRVNDgfuBJUBZ5TzoiLPY7HEHyzbicXeChEbYKI';
--- src/lib/types.ts
+++
@@ -1,7 +0,0 @@
-export interface Talk {
-	title: string;
-	date: string;
-	slides: string;
-	video: string;
-	code: string;
-}

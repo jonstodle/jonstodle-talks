9e185e22d0d1f06ee454d6b2293fdafcfb073141
Jon Grythe Stødle <jon@stodle.net>
2023-03-04 01:35:44 +0100

    Add time to dates

--- migrations/0000_Initial.sql
+++ migrations/0000_Initial.sql
@@ -3,26 +3,27 @@
 create table talks
 (
     id     integer primary key autoincrement,
-    title  text not null,
-    date   date not null,
+    title  text     not null,
+    date   datetime not null,
     slides text,
     video  text,
     code   text
 );
 
 insert into talks (title, date, slides, video, code)
-values ('Getting Started with Svelte - Cybernetically Enhanced Web Apps', '2020-05-01',
+values ('Getting Started with Svelte - Cybernetically Enhanced Web Apps', '2020-05-01T00:00:00.00Z',
         'https://drive.google.com/file/d/15L1vvQFSvl1Zhmr4SLI_kWmJTe0Jh3Wa/view?usp=sharing',
         'https://youtu.be/eK27uMco3UI', null),
-       ('Let''s Go! En praktisk innføring i Go(lang)', '2022-11-19', null, null,
+       ('Let''s Go! En praktisk innføring i Go(lang)', '2022-11-19T00:00:00.00Z', null, null,
         'https://github.com/jonstodle/lets-go'),
-       ('Svelte – Bedre for utvikleren og raskere for brukeren', '2022-04-30', null, null,
+       ('Svelte – Bedre for utvikleren og raskere for brukeren', '2022-04-30T00:00:00.00Z', null, null,
         'https://github.com/jonstodle/svelte-todo'),
-       ('Svelte – Bedre for utvikleren og raskere for brukeren, eller just another javascript framework?', '2021-11-11',
+       ('Svelte – Bedre for utvikleren og raskere for brukeren, eller just another javascript framework?',
+        '2021-11-11T00:00:00.00Z',
         null, null, 'https://github.com/jonstodle/svelte-todo'),
-       ('Take off with Flutter', '2018-11-01', 'https://1drv.ms/p/s!Aoq2gL8AZ0zfrqp_QO-oHl6eFyqbEg',
+       ('Take off with Flutter', '2018-11-01T00:00:00.00Z', 'https://1drv.ms/p/s!Aoq2gL8AZ0zfrqp_QO-oHl6eFyqbEg',
         'https://youtu.be/VpSRGlgVXrE', null),
-       ('Intro to Docker', '2018-08-01', null, null, 'https://github.com/jonstodle/WebDevDocker'),
-       ('RxJS', '2017-05-01', 'https://slides.com/jonstodle/rxjs', null, null),
-       ('Realm', '2017-03-01', 'https://slides.com/jonstodle/realm-eng#/', null,
+       ('Intro to Docker', '2018-08-01T00:00:00.00Z', null, null, 'https://github.com/jonstodle/WebDevDocker'),
+       ('RxJS', '2017-05-01T00:00:00.00Z', 'https://slides.com/jonstodle/rxjs', null, null),
+       ('Realm', '2017-03-01T00:00:00.00Z', 'https://slides.com/jonstodle/realm-eng#/', null,
         'https://github.com/jonstodle/RealmTodo');
